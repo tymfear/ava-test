352e80e2bfbea2cb44fa4c88ee4733b27c37029d
Timur Daudov <timur.daudov@namecheap.com>
2019-05-05 02:45:18 +0300

    as

--- test/test.1.ts
+++ test/test.1.ts
@@ -6,8 +6,7 @@ const instance: AxiosInstance = axios.create();
 instance.interceptors.request.use(AxiosLogger.requestLogger, AxiosLogger.errorLogger);
 instance.interceptors.response.use(AxiosLogger.responseLogger, AxiosLogger.errorLogger);
 
-test('[21312] Another test 2', async (t: ExecutionContext) => {
-  const result: AxiosResponse = await instance('http://ipv4.icanhazip.com');
-
-  t.is(result.data, 'asd');
+test.skip('[1221341234] Skipped test', async (t) => {
+  await instance('http://ipv4.icanhazip.com');
+  t.pass();
 });
--- test/test.2.ts
+++ test/test.2.ts
@@ -6,8 +6,11 @@ const instance: AxiosInstance = axios.create();
 instance.interceptors.request.use(AxiosLogger.requestLogger, AxiosLogger.errorLogger);
 instance.interceptors.response.use(AxiosLogger.responseLogger, AxiosLogger.errorLogger);
 
-test('[21312] Another test 2', async (t: ExecutionContext) => {
-  const result: AxiosResponse = await instance('http://ipv4.icanhazip.com');
+test.before(() => {
+  throw Error('adad');
+});
 
-  t.is(result.data, 'asd');
+test('[12341234] Failed before', async (t) => {
+  await instance('http://ipv4.icanhazip.com');
+  t.pass();
 });
